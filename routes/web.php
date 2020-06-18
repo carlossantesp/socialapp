9535fa0d6fd09892ef82fe9d439d8e9a3b6c68ae
Carlos Santillan <carlos.santesp@gmail.com>
2020-06-18 18:09:30 -0400

    ADD: Diseño de status con bootstrap

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'welcome');
 
 Route::post('statuses', 'StatusController@store')->name('statuses.store');
 
